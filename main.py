3873dd918dd147d61e94d715e2c092de4a4cfd62
Iiro <van.hatlas@gmail.com>
2023-12-20 12:33:53 +0200

    bookbot finished

---
+++ main.py
@@ -0,0 +1,48 @@
+def main():
+   book_path = "books/frankenstein.txt"
+   text = get_book_text(book_path)
+   num_words = count_words(text)
+   chars_dict = count_chars(text)
+   chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+   
+   print(f"--- Begin report of books/frankenstein.txt ---")
+   print(f"{num_words} words found in the document\n")
+   
+   for item in chars_sorted_list:
+      if not item["char"].isalpha():
+         continue
+      print(f"The '{item['char']}' character was found {item['num']} times")
+   
+   print("--- End report ---")
+   
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def count_chars(text):
+   chars = {}
+   for f in text:
+      lowered = f.lower()
+      if lowered in chars:
+         chars[lowered] += 1
+      else:
+         chars[lowered] = 1   
+   return chars     
+
+def get_book_text(path):
+   with open(path) as f:
+      return f.read()
+
+
+main()      
+
